7767fa62a3c2b5800980a7a5a104ae07facdc0f0
Egtra <yusuke.ichinohe@gmail.com>
2011-10-23 05:19:25 +0900

    Add Visual C++ 2010 project for notify

--- cpp/test/notify.cc
+++ cpp/test/notify.cc
@@ -1,13 +1,25 @@
+#ifdef _WIN32
+#	include "wsainitializer.h"
+#endif
+
 #include "echo_server.h"
 #include <msgpack/rpc/server.h>
 #include <msgpack/rpc/client.h>
 #include <cclog/cclog.h>
-#include <cclog/cclog_tty.h>
+#ifdef _WIN32
+#	include <cclog/cclog_console.h>
+#else
+#	include <cclog/cclog_tty.h>
+#endif
 
 int main(void)
 {
+#ifdef _WIN32
+	cclog::reset(new cclog_console(cclog::TRACE, ::GetStdHandle(STD_OUTPUT_HANDLE)));
+#else
 	cclog::reset(new cclog_tty(cclog::TRACE, std::cout));
 	signal(SIGPIPE, SIG_IGN);
+#endif
 
 	// run server {
 	rpc::server svr;
@@ -31,7 +43,11 @@ int main(void)
 
 	cli.get_loop()->flush();
 
+#ifdef _WIN32
+	Sleep(10);
+#else
 	usleep(100000);
+#endif
 
 	return 0;
 }
