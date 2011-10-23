d83ecde87b0eb406e14681c24d2ee8e1668411ed
Egtra <yusuke.ichinohe@gmail.com>
2011-10-23 14:51:09 +0900

    Add Visual C++ 2010 project for async_server

--- cpp/test/async_server.cc
+++ cpp/test/async_server.cc
@@ -1,8 +1,16 @@
+#ifdef _WIN32
+#	include "wsainitializer.h"
+#endif
+
 #include "echo_server.h"
 #include <msgpack/rpc/server.h>
 #include <msgpack/rpc/client.h>
 #include <cclog/cclog.h>
-#include <cclog/cclog_tty.h>
+#ifdef _WIN32
+#	include <cclog/cclog_console.h>
+#else
+#	include <cclog/cclog_tty.h>
+#endif
 
 using namespace mp::placeholders;
 
@@ -40,8 +48,12 @@ private:
 
 int main(void)
 {
+#ifdef _WIN32
+	cclog::reset(new cclog_console(cclog::TRACE, ::GetStdHandle(STD_OUTPUT_HANDLE)));
+#else
 	cclog::reset(new cclog_tty(cclog::TRACE, std::cout));
 	signal(SIGPIPE, SIG_IGN);
+#endif
 
 	// run server {
 	rpc::server svr;
